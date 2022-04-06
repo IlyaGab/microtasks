97950a06159f7443e28ee78d86d5d98f8bf5f489
IlyaGab <ilyagab1994@gmail.com>
2022-04-06 19:40:13 +0300

    add content

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [message, setMessage] = useState([
+    {message: 'message1'},
+    {message: 'message2'},
+    {message: 'message3'}
+  ])
   return (
     <div className="App">
-
+      <div>
+        <input/>
+        <button>+</button>
+      </div>
+      {message.map((el, index)=> {
+        return (
+            <div key={index}>{el.message}</div>
+        )
+      })}
     </div>
   );
 }
